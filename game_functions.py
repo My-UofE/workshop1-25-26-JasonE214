08ffcf1fc81ff12c40773d5ea10be80aac0f0d5b
JasonE214 <khc214@exeter.ac.uk>
2026-01-16 15:57:34 +0000

    fixing q3

--- game_functions.py
+++ game_functions.py
@@ -24,4 +24,4 @@ def process_guess(letter, board, word):
         if l.lower() == letter.lower():
             guess = True
             board[i] = letter
-    return guess, board
+    return guess,board
